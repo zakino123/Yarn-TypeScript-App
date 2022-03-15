f16c4e49496b03684d955828451bd4d5b809fbe3
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-15 10:51:29 +0900

    導入したパッケージの活用

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-import { addBaz } from './baz';
+import { addBaz } from "./baz.js";
 window.onload = function () {
     const app = document.getElementById("foo");
     app.innerHTML = "Hello Typescript World! foo";
